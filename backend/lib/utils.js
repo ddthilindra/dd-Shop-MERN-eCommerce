0a49c4342bf48348dbcf59552945ee2a986deaef
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-04 16:44:56 +0530

    Generate a JSON Web Token and Custom Authentication Middleware

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -4,26 +4,26 @@ import User from '../models/user.model.js'
 
 const generateAuthToken=(user)=> {
     
-  const expiresIn = "2w";
+    const payload = {
+        sub: {
+            id: user._id,
+            verified:true,
+        },
+        iat: Date.now(),
+    };
 
-  const payload = {
-    sub: {
-      _id: user._id,
-      verified:true,
-    },
-    iat: Date.now(),
-  };
+    const expiresIn = "2w";
 
-  const signedToken = jwt.sign( payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: expiresIn});
+    const signedToken = jwt.sign( payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: expiresIn});
 
-  return {
-    token: "Bearer " + signedToken,
-    expires: expiresIn,
-    // sub: {
-    //   _id: user._id,
-    //   verified:true,
-    // },
-  };
+    return {
+        token: "Bearer " + signedToken,
+        expires: expiresIn,
+        // sub: {
+        //   _id: user._id,
+        //   verified:true,
+        // },
+    };
   
 }
 
@@ -31,13 +31,15 @@ const authMiddleware=asyncHandler(async(req, res, next) =>{
     
         if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
           const tokenParts = req.headers.authorization.split(' ');
+
           if (tokenParts[0] === 'Bearer' && tokenParts[1].match(/\S+\.\S+\.\S+/) !== null) {
+            
             try {
               const verification = jwt.verify(tokenParts[1], process.env.ACCESS_TOKEN_SECRET);
               if(verification.sub.verified === false){
                 res.status(401).json({ success: false, status: 'Unauthorized', msg: "You are not verified your email to access your account" });
               }else{
-                req.user = await User.findById(verification.sub._id).select('-password')
+                req.user = await User.findById(verification.sub.id).select('-password')
                 req.jwt = verification;
                 next();
               }
