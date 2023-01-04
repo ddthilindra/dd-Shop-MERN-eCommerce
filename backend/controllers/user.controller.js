d26148a544f34a9085ab7a354ab07e530dee19e4
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-04 17:37:33 +0530

    User Registration & Password Encryption

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,38 +5,74 @@ import User from '../models/user.model.js';
 // @desc     Auth user & get token
 // @route    POST /api/users/login
 // @access   Public
-export const authUser= asyncHandler(async(req,res)=>{
-   const {email,password} = req.body;
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-   const user=await User.findOne({email})
+  const user = await User.findOne({ email });
 
-   if(user && (await user.matchPassword(password))){ // matchPassword is checking entered password is correct in user mode
+  if (user && (await user.matchPassword(password))) {
+    // matchPassword is checking entered password is correct in user mode
     res.json({
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        isAdmin:user.isAdmin,
-        token:generateAuthToken(user),
-    })
-   }else{
-    res.status(401)
-    throw new Error('Invalid email or password')
-   }
-})
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateAuthToken(user),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
 
+// @desc     Register new user
+// @route    POST /api/users/
+// @access   Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name,email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exist!');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateAuthToken(user),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc     Get user profile
+// @route    GET /api/users/profile
+// @access   Private
 export const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
-  
-    if (user) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-      })
-    } else {
-      res.status(404)
-      throw new Error('User not found')
-    }
-  })
-  
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
--- backend/routes/userRoute/user.route.js
+++ backend/routes/userRoute/user.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../../controllers/user.controller.js';
+import { authUser, getUserProfile, registerUser } from '../../controllers/user.controller.js';
 import { authMiddleware } from '../../lib/utils.js';
 
-// @route    POST POST /api/users/login
+// @route    POST /api/users/register
+router.post('/', registerUser);
+// @route    POST /api/users/login
 router.post('/login', authUser);
-// @route    POST POST /api/users/login
+// @route    POST /api/users/profile
 router.get('/profile',authMiddleware, getUserProfile);
 
 export default router;
