518d96a636ff0b8695ef18001b281026619dc01b
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-06 23:08:43 +0530

    Order Controller & Route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const routes = express.Router();
-import productRoutes from './productRoute/product.route.js';
 import userRoutes from './userRoute/user.route.js';
+import productRoutes from './productRoute/product.route.js';
+import orderRoutes from './orderRoute/order.route.js';
 
-routes.use('/api/products', productRoutes);
 routes.use('/api/users', userRoutes);
+routes.use('/api/products', productRoutes);
+routes.use('/api/orders', orderRoutes);
 
 export default  routes;
