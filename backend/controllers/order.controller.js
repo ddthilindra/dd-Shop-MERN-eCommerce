5b90dc77d648c3151e123d3d9c1e4de1d929cb91
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-08 19:10:08 +0530

    Update To Paid Endpoint

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -49,3 +49,30 @@ export const getOrderById = asyncHandler(async (req, res) => {
     throw new Error('Order not found..!')
   }
 });
+
+// @desc     Update order to [aid]
+// @route    GET /api/orders/:id/pay
+// @access   Private
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order=await Order.findById(req.params.id)
+
+  if(order){
+    // set the properties
+    order.isPaid=true
+    order.paidAt=Data.now()
+    // comming from paypal
+    order.paymentResult={
+      id:req.body.id,
+      status:req.body.status,
+      update_time:req.body.update_time,
+      email_address:req.body.email_address,
+    }
+
+    const updatedOrder=await order.save()
+
+    res.json(updatedOrder)
+  }else{
+    res.status(404)
+    throw new Error('Order not found..!')
+  }
+});
--- backend/routes/orderRoute/order.route.js
+++ backend/routes/orderRoute/order.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderById } from '../../controllers/order.controller.js';
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../../controllers/order.controller.js';
 import { authMiddleware } from '../../lib/utils.js';
 
 // @route    POST /api/orders
 router.post('/',authMiddleware, addOrderItems);
 // @route    GET /api/orders/:id
 router.get('/:id',authMiddleware, getOrderById);
+// @route    PUT /api/orders/:id/pay
+router.put('/:id/pay',authMiddleware, updateOrderToPaid);
 
 export default router;
