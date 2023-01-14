eab8d032fed1ddfb5e603e1349cf6d4348e06787
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-14 23:22:12 +0530

    Delete Product By ID for Admin Endpoint

--- backend/routes/productRoute/product.route.js
+++ backend/routes/productRoute/product.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { getProductById, getProducts } from '../../controllers/product.controller.js';
-
+import { deleteProduct, getProductById, getProducts } from '../../controllers/product.controller.js';
+import { authMiddleware,adminAuthMiddleware } from '../../lib/utils.js';
 
 // @route    GET /api/products
 router.get('/', getProducts);
 
 // @route    GET /api/products/:id
 router.get('/:id', getProductById);
+// @route    DELETE /api/products/:id
+router.delete('/:id',authMiddleware,adminAuthMiddleware, deleteProduct);
 
 export default router;
