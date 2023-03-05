2c9e13ff099edc025725461bcd104707a15d5e88
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-03-05 18:34:24 +0530

    Prepare For Deployment | Server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import path from 'path';
 import morgan from 'morgan';
-import { notFound,errorHandler } from './middleware/error.middleware.js';
+import { notFound, errorHandler } from './middleware/error.middleware.js';
 
 import indexRoute from './routes/index.js';
 
@@ -13,25 +13,36 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-if(process.env.NODE_ENV==='development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
-app.use("/", indexRoute);
+app.use('/', indexRoute);
 
 // make upload folder static in root
-const __dirname =path.resolve()
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  // make frontend/build folder as static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // get anything that is'nt any of API route in production
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) // point to static folder
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 
 const PORT = process.env.PORT || 5000;
 
-app.use(notFound)
+app.use(notFound);
 
 // Handle the 500 html error with json
 app.use(errorHandler);
